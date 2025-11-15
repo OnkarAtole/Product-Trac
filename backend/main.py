446f01bbadd59318d2bd050972fac2488dfa794e
OnkarAtole <atoleonkar24@gmail.com>
2025-11-15 18:41:50 +0530

    upload6

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ load_dotenv()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # No trailing slash
+    allow_origins=["FRONTEND_URL"],  # No trailing slash
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods: GET, POST, PUT, DELETE
     allow_headers=["*"],  # Allow all headers
